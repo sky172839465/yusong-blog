fd3cabb892806344784ca3a03cb1c72864b21835
Yusong Hsu <sky172839465@gmail.com>
2019-07-27 10:33:35 +0800

    feat: add NetlifyCMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `Yusong Hsu`
   },
   plugins: [
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
