d5c17c439521e26874c17f9ca081985667dd521c
Yusong Hsu <sky172839465@gmail.com>
2019-07-30 20:47:37 +0800

    refactor: rename logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/yusong.io.favicon.png` // This path is relative to the root of the site.
+        icon: `src/images/yusong.io.png` // This path is relative to the root of the site.
       }
     },
     `gatsby-plugin-react-helmet`,
