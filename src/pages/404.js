5076c38e5deafc2538e4aab17fd44a8fef283119
Yusong Hsu <sky172839465@gmail.com>
2020-02-22 13:56:26 +0800

    fix: jsx-a11y/accessible-emoji

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,12 @@ import Title from '../components/title'
 const NotFoundPage = () => (
   <main>
     <SEO title='404: Not found' />
-    <Title>NOT FOUND <span>👻</span></Title>
-    <p>You just hit a route that doesn&#39;t exist... <span>😢</span></p>
+    <Title>
+      NOT FOUND <span role='img' aria-label='ghost'>👻</span>
+    </Title>
+    <p>
+      You just hit a route that doesn&#39;t exist... <span role='img' aria-label='sad'>😢</span>
+    </p>
   </main>
 )
 
