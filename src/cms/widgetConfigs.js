808911bc17f098e47a7990a9690512a32c20affc
Yusong Hsu <sky172839465@gmail.com>
2021-11-30 00:34:30 +0800

    feat: add netlifycms mdx support

--- src/cms/widgetConfigs.js
+++ src/cms/widgetConfigs.js
@@ -1,5 +1,7 @@
 import { Control } from './widget/postUrl'
+import { MdxControl, MdxPreview } from 'netlify-cms-widget-mdx'
 
 export default [
+  ['mdx', MdxControl, MdxPreview],
   ['postUrl', Control]
 ]
